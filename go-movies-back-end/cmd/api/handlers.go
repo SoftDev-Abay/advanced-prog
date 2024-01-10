30842b2a81f0aeb298f2619363feca6ee00161ca
cowbuno_fedora <talipov.zhenis@gmail.com>
2024-01-10 14:27:25 +0600

    connect-db-movie-finish

--- go-movies-back-end/cmd/api/handlers.go
+++ go-movies-back-end/cmd/api/handlers.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -29,38 +27,17 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
-	var movies []models.Movie
+	movies, err := app.DB.AllMovies()
 
-	rd, _ := time.Parse("2006-01-02", "2023-03-7")
-
-	spiderman := models.Movie{
-		ID:           1,
-		Title:        "Spiderman",
-		ReleaseDate:  rd,
-		MPAARating:   "R",
-		RunTime:      120,
-		Description:  "Something excellent",
-		CreatedAt:    time.Now(),
-		UpdatedField: time.Now(),
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-
-	batman := models.Movie{
-		ID:           2,
-		Title:        "Batman",
-		ReleaseDate:  rd,
-		MPAARating:   "PG-13",
-		RunTime:      120,
-		Description:  "Very cool movie excellent",
-		CreatedAt:    time.Now(),
-		UpdatedField: time.Now(),
-	}
-
-	movies = append(movies, spiderman)
-	movies = append(movies, batman)
 
 	out, err := json.Marshal(movies)
 	if err != nil {
-		fmt.Println()
+		fmt.Println(err)
+		return
 	}
 	w.Header().Set("Connect-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
--- go-movies-back-end/cmd/api/main.go
+++ go-movies-back-end/cmd/api/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"database/sql"
+	"backend/internal/repository"
+	"backend/internal/repository/dbrepo"
 	"flag"
 	"fmt"
 	"log"
@@ -13,7 +14,7 @@ const port = 8080
 type application struct {
 	DSN    string
 	Domain string
-	DB     *sql.DB
+	DB     repository.DatabaseRepo
 }
 
 func main() {
@@ -30,7 +31,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	app.DB = conn
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer conn.Close()
 
 	// start a web server
 	fmt.Println("Stargin app on port", port)
